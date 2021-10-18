42d10978dd252a2422c49134d83ca0fdbf0411b2
gabbyMill <gabbymillatiner@gmail.com>
2021-10-18 14:41:12 +0300

    Fixed Function

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const moment = require("moment");
 
 const timeToNow = dateArray => {
-  let correctFormat = "";
-  dateArray.forEach(x => (correctFormat += x));
-  return moment(correctFormat, "YYYYMMDD").fromNow();
+  return moment(dateArray.join(""), "YYYYMMDD").fromNow();
 };
 console.log(timeToNow(["1921", "08", "18"]));
